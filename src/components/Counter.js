bf59aa6b6b0bfaade47797273f0e32ff4125acab
NF Tushar <58363866+nftushar@users.noreply.github.com>
2022-11-27 22:50:38 +0600

    React assigment - 2 : Counter App Done
    useState() hook
    Event Handler  &
    conditional rendering,
    increment, decrement and reset functionalities are working fine,
    increment button disable when the count value is 5 using disable attribute   &
     decrement button disable when the count value is -5 using disable attribute,
    Thank you, sir

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button className="btn card__btn" disabled={count === -5} onClick={() => setCount(count - 1)}> - </button>
+          <button className="btn card__btn" disabled={count === 5} onClick={() => setCount(count + 1)}> + </button>
+          <button className="btn card__btn" onClick={() => setCount(0)}>0</button>
         </div>
       </div>
     </div>
   );
-};
 
+};
 export default Counter;
